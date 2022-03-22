ac3b7a0d99bfb3a114c49d544add1bb53fbd561f
Benedikt Claus <benediktclaus@msn.com>
2022-03-22 10:53:53 +0100

    Add example dataset (Claus et al., 2020)

---
+++ R/datasets.R
@@ -0,0 +1,27 @@
+#' Placebo Amplification Data
+#'
+#' A dataset containing the data from Claus et al. (2020). In a routine
+#' inpatient setting for unipolar depressive disorders they implemented an
+#' intervention that sought to amplify the placebo response of antidepressants.
+#' In the study, two groups were compared: treatment as usual (TAU) and placebo
+#' amplification (PA). Participants were examined four times during their
+#' treatment.
+#'
+#' @format An object of class `tbl_df` with 172 rows and 9 columns.
+#'
+#'   \describe{ \item{id}{Participant ID} \item{age}{Age} \item{sex}{Sex}
+#'   \item{treatment}{Treatment (TAU for treatment as usual and PA for placebo
+#'   amplification)} \item{time}{Measurement} \item{bdi}{Beck Depression
+#'   Inventory (2nd Edition) score} \item{shaps}{Snaith-Hamilton Pleasure Scale
+#'   score} \item{who}{WHO-Five Well-Being Index score} \item{hamd}{Hamilton
+#'   Rating Scale for Depression score} }
+#'
+#' @references - Claus, B. B., Scherbaum, N., & Bonnet, U. (2020). Effectiveness
+#'   of an Adjunctive Psychotherapeutic Intervention Developed for Enhancing the
+#'   Placebo Effect of Antidepressants Used within an Inpatient-Treatment
+#'   Program of Major Depression: A Pragmatic Parallel-Group, Randomized
+#'   Controlled Trial. Psychotherapy and Psychosomatics, 89(4), 258-260.
+#'   https://doi.org/10.1159/000505855
+#'
+#' @source \url{https://osf.io/rc754/}
+"claus_2020"
---
+++ data-raw/DATASET.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(readxl)
+library(janitor)
+
+
+
+# Placebo Amplification ---------------------------------------------------
+# Dataset from Claus et al., (2020)
+# https://osf.io/rc754/
+claus_import <- read_excel("data-raw/claus-2020.xlsx") %>%
+  select(id, age, sex, treatment, matches("(BDI|HAMD|SHAPS|WHO).*total$")) %>%
+  clean_names()
+
+claus_2020 <- claus_import %>%
+  pivot_longer(
+    cols = bdi1_total:hamd4_total,
+    names_to = c(".value", "time"),
+    names_pattern = "(^.*)(\\d{1})"
+  ) %>%
+  mutate(
+    sex = factor(sex, levels = 0:1, labels = c("Female", "Male")),
+    treatment = factor(treatment, levels = 0:1, labels = c("TAU", "PA")),
+    time = as.numeric(time)
+  ) %>%
+  as_tibble()
+
+use_data(claus_2020, overwrite = TRUE)

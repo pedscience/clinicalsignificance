5cabf2ee312f94a8a898d8042e11d9d9c53e1910
Benedikt Claus <benediktclaus@msn.com>
2022-03-24 12:21:34 +0100

    Move checks up to parent function

--- R/prep_cutoff.R
+++ R/prep_cutoff.R
@@ -1,15 +1,4 @@
-prep_cutoff <- function(data, m_functional = NA, sd_functional = NA, type = "c", better_is = c("lower", "higher")) {
-  # Check if all necessary information is provided
-  if (type != "a" & (missing(m_functional) | missing(sd_functional))) {
-    stop(paste0("To calculate cutoff ", type, ", summary statistics of the functional population must be defined."))
-  }
-
-  # If type = "a", discard information of the functional population and give a warning
-  if (type == "a" & (!is.na(m_functional) | !is.na(sd_functional))) {
-    m_functional <- sd_functional <- NA_real_
-    warning("You selected cutoff type \"a\" and provided summary statistics for the functional population. This information will be dicarded.\nIf you wand to incorporate data from the functional population, choose type = \"b\" or \"c\"", call. = FALSE)
-  }
-
+prep_cutoff <- function(data, m_functional, sd_functional, type, better_is) {
 
   # If type = "a" or "c", calculate mean and standard deviation based on the
   # data. Otherwise, these will be NA

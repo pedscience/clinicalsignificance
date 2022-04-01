4908da2c00008929fcf9479edd36ecf5dfcea455
Benedikt Claus <benediktclaus@msn.com>
2022-04-01 13:16:19 +0200

    In get_summary_table add an option which table to return for HA. Check is OK

--- R/get_summary.R
+++
@@ -1,9 +0,0 @@
-#' Get A Summary Table From A clinisig Object
-#'
-#' @inheritParams get_data
-#'
-#' @return A tibble with clinical significance categories
-#' @export
-get_summary <- function(x) {
-  x[["summary"]]
-}

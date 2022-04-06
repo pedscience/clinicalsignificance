f214e6259374b0cbd4682a24ca53d609b1b7a0a2
Benedikt Claus <benediktclaus@msn.com>
2022-04-06 15:49:29 +0200

    Include indiviual category for plotting

--- R/get_augmented_data.R
+++ R/get_augmented_data.R
@@ -2,7 +2,7 @@
 #'
 #' @inheritParams get_data
 #'
-#' @importFrom dplyr left_join
+#' @importFrom dplyr left_join case_when
 #'
 #' @return A tibble with used data and clinical significance categories
 #' @export
@@ -10,5 +10,15 @@ get_augmented_data <- function(x) {
   used_data <- x[["datasets"]][["data"]]
   categories <- x[["categories"]]
 
-  left_join(used_data, categories, by = "id")
+  left_join(used_data, categories, by = "id") %>%
+    mutate(
+      category = case_when(
+        recovered ~ "Recovered",
+        improved ~ "Improved",
+        unchanged ~ "Unchanged",
+        deteriorated ~ "Deteriorated",
+        harmed ~ "Harmed"
+      ),
+      category = factor(category, levels = c("Recovered", "Improved", "Unchanged", "Deteriorated", "Harmed"))
+    )
 }

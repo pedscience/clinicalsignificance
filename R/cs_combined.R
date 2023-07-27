eaa13ce59ba4b812012e70d4dadcc3c4af5889dc
Benedikt Claus <benediktclaus@msn.com>
2023-07-27 11:28:04 +0200

    Begin cs_combined

---
+++ R/cs_combined.R
@@ -0,0 +1,18 @@
+cs_combined <- function(data,
+                        id,
+                        time,
+                        outcome,
+                        group = NULL,
+                        pre = NULL,
+                        post = NULL,
+                        reliability = NULL,
+                        reliability_post = NULL,
+                        m_functional = NULL,
+                        sd_functional = NULL,
+                        reliability = NULL,
+                        better_is = c("lower", "higher"),
+                        rci_method = c("JT", "GLN", "HLL", "EN", "NK", "HA", "HLM"),
+                        cutoff_type = c("a", "b", "c"),
+                        significance_level = 0.05) {
+
+}

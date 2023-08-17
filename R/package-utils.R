8baf95a0e860397f3ebf4e16667a9b74b3fe78cd
Benedikt Claus <benediktclaus@msn.com>
2023-08-17 13:44:31 +0200

    Delete unnecessary files

--- R/package-utils.R
+++ R/package-utils.R
@@ -1,3 +1,29 @@
+#' Check if a data frame has a column named group
+#'
+#' @param data A data frame
+#'
+#' @return Boolean
+#'
+#' @noRd
+.has_group <- function(data) {
+  "group" %in% names(data)
+}
+
+
+
+#' Check if an Object is of Class cs_analysis
+#'
+#' @param data A data frame
+#'
+#' @return Boolean
+#'
+#' @noRd
+.check_class <- function(x) {
+  if (!inherits(x, "cs_analysis")) cli::cli_abort("The supplied object must be of class {.code cs_analysis} and not {.code {class(x)}}.")
+}
+
+
+
 #' Calculate improvement or deterioration based on RCI scores
 #'
 #' This functions expects at least a preprocessed data frame with the column
